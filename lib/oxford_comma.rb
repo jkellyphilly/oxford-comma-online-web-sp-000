ab68f46f87fa468a3e78ffe443f8fa4e853a471e
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-24 20:55:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   if array.size == 1
-    return array[0]
+    array[0]
   elsif array.size == 2
-    return array[0] << " and " << array[1]
+    array[0] << " and " << array[1]
+  else
+    puts "The rest of my code here"
   end
-  puts "The rest of my code here"
 end
